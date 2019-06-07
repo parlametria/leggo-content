3d7999778e1db0e58bb73cdf2187aa35e00ecc66
Jair Guedes <jairguedesferreira@gmail.com>
2019-06-07 09:39:13 -0300

    Melhora o script que verifica se o texto é imagem

--- util/data/verifica_se_pdf_imagem.py
+++ util/data/verifica_se_pdf_imagem.py
@@ -11,7 +11,7 @@ for subdir, dirs, files in os.walk(data_links):
     for file in files:
         file_path = os.path.join(subdir, file)
         #Apaga os txts gerados por pfds que eram imagens
-        if os.stat(file_path).st_size < 100 or 'Document Outline' in open(file_path).read():
+        if os.stat(file_path).st_size < 100:
             os.remove(file_path)
             print "Removing file: " + file_path
             f.write("Removing file: " + file_path + "\r\n")
