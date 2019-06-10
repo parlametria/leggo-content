465cdc432b2f1cc4ff0d248a1c06b1f21053f10b
Jair Guedes <jairguedesferreira@gmail.com>
2019-06-10 10:34:35 -0300

    Ajusta o script que verifica o tamanho do arquivo

--- util/data/calibre_convert.sh
+++ util/data/calibre_convert.sh
@@ -7,8 +7,10 @@ for folder in $(ls $DIR_DATA/); do
 		filename="${f%.*}"
 		for g in $(ls $DIR_DATA/$folder/pdf/$f); do
 		      ebook-convert $DIR_DATA/$folder/pdf/$f $DIR_DATA/$folder/txt/$filename.txt
-		      if [ wc -c $DIR_DATA/$folder/txt/$filename.txt | awk '{print $1}' -lt 100 ]
+                      fileSize=`wc -c $DIR_DATA/$folder/txt/$filename.txt | cut -d' ' -f1`
+		      if [ $fileSize -lt 100 ] 
 			  then
+				echo $DIR_DATA/$folder/txt/$filename.txt
 			  	rm $DIR_DATA/$folder/txt/$filename.txt
 			  fi
 		done
