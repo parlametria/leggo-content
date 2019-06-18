6ddd71d4d49ff6045f849461b70b151319d63643
Jair Guedes <jairguedesferreira@gmail.com>
2019-06-18 14:44:02 -0300

    Refatora script de separar justificações

--- util/tools/SepararJustificacoes.py
+++ util/tools/SepararJustificacoes.py
@@ -43,14 +43,17 @@ else:
             
                 with open(os.path.normpath(os.path.join(dirpath,filename)), 'r', encoding = 'utf-8') as pl:
                     ProjetoDeLei = pl.read()
-                
-                    if re.search(pat,ProjetoDeLei) or re.search(pat2,ProjetoDeLei):
-                        justificacao = re.split(r"\njustificação\n", ProjetoDeLei, maxsplit = 1, flags = re.IGNORECASE)[0]
-                        with open(newPath + os.path.splitext(filename)[0] + '.txt', 'w',encoding = 'utf-8') as j:
-                            j.write(justificacao)
-                     elif re.search(pat3, ProjetoDeLei):
-                        with open(newPath + os.path.splitext(filename)[0] + '.txt', 'w',encoding = 'utf-8') as j:
-                            j.write(ProjetoDeLei)
+                    if re.search(pat,ProjetoDeLei):	
+                        textoParaEscrever = re.split(r"\njustificação\n", ProjetoDeLei, maxsplit = 1, flags = re.IGNORECASE)[0]
+                    elif re.search(pat2,ProjetoDeLei):	
+                        textoParaEscrever = re.split(r"\njustificativa\n", ProjetoDeLei, maxsplit = 1, flags = re.IGNORECASE)[0]
+                    elif re.search(pat3,ProjetoDeLei):	
+                        textoParaEscrever = ProjetoDeLei
+                    else:
+                        continue
+                    with open(newPath + os.path.splitext(filename)[0] + '.txt', 'w',encoding = 'utf-8') as j:
+                        j.write(textoParaEscrever)
+                    
                     
 
                     
