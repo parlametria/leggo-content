1412049c42728ad85f246c56feeb858a58dbe86c
Ubuntu <ubuntu@agora-frontend.lsd.ufcg.edu.br>
2019-07-05 10:15:53 -0300

    Adiciona tratamento de exceção ao script que baixa os PDFs dos arquivos.

--- util/data/download_csv_prop.py
+++ util/data/download_csv_prop.py
@@ -3,6 +3,7 @@ import os
 import sys
 import urllib.request
 import unidecode
+import traceback
 
 def print_usage():
   print("Número errado de parâmetros,o certo é: donwload_csv_props.py <caminho_csv_links_arquivos> <output_dir>")
@@ -13,26 +14,30 @@ def download_pdfs_from_path(csv_path):
   count_tipo = {}
   
   for idx, row in df.iterrows():
+
+      try:
   
-      link = row["link_inteiro_teor"]
-      id_proposicao = str(row["id_proposicao"])
-      cd_texto = str(row["codigo_texto"])
+        link = row["link_inteiro_teor"]
+        id_proposicao = str(row["id_proposicao"])
+        cd_texto = str(row["codigo_texto"])
   
-      full_outputpath = os.path.join(output_dir, id_proposicao)
+        full_outputpath = os.path.join(output_dir, id_proposicao)
   
-      if not(os.path.exists(full_outputpath)):
+        if not(os.path.exists(full_outputpath)):
           os.mkdir(full_outputpath)
   
-      full_outputpath = os.path.join(full_outputpath, "pdf")
-      if not(os.path.exists(full_outputpath)):
+        full_outputpath = os.path.join(full_outputpath, "pdf")
+        if not(os.path.exists(full_outputpath)):
           os.mkdir(full_outputpath)
   
-      tipo = unidecode.unidecode(row["tipo_texto"].lower())
+        tipo = unidecode.unidecode(row["tipo_texto"].lower())
   
-      file_name_tipo = "%s_%s_%s.pdf" % (cd_texto, id_proposicao, tipo.replace(" ","_"))
-      file_name = os.path.join(full_outputpath, file_name_tipo)
-      print(file_name)
-      urllib.request.urlretrieve(link, file_name)
+        file_name_tipo = "%s_%s_%s.pdf" % (cd_texto, id_proposicao, tipo.replace(" ","_"))
+        file_name = os.path.join(full_outputpath, file_name_tipo)
+        print(file_name)
+        urllib.request.urlretrieve(link, file_name)
+      except:
+          print(traceback.format_exc())
 
 if (len(sys.argv) != 3):
   print_usage()
