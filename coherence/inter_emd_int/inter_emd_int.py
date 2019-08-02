f67b86e46b01bbdfa7480e3a9a8b3ac34dbf721f
Jair Guedes <jairguedesferreira@gmail.com>
2019-08-02 16:25:32 -0300

    Separa as frases corretamente

--- coherence/inter_emd_int/inter_emd_int.py
+++ coherence/inter_emd_int/inter_emd_int.py
@@ -66,7 +66,7 @@ for file in files:
         tokenized_sentences = []
         line = re.sub(r'[^\w\d\s]+', '', line)
         tokenized_sentences.append(line.lower().split())
-        stop_words = set(stopwords.words('portuguese') + list(punctuation))
+        stop_words = set(stopwords.words('portuguese') + list(punctuation) + ['art', 'arts', 'nº', 'nr'])
         for t in tokenized_sentences:
             emdSentences.append([w for w in t if w not in stop_words])
 
@@ -91,15 +91,23 @@ if intFile == "":
 
 finalTokenizedSentences = []
 intSentences = []
-for line in intFile:
-    tokenized_sentences = []
-    line = re.sub(r'[^\w\d\s]+', '', line)
-    tokenized_sentences.append(line.lower().split())
-    stop_words = set(stopwords.words('portuguese') + list(punctuation))
-    for t in tokenized_sentences:
-        intSentences.append([w for w in t if w not in stop_words])
 
-# In[12]:
+
+sub = re.split("(\r\n|\r|\n|“)+Art|(\r\n|\r|\n)+§",  str(intFile.read()), flags=re.IGNORECASE)
+for line in sub:
+    if (line != None and line.strip() != ""):
+        tokenized_sentences = []
+        line = str(line)
+        line = re.sub(r'[^\w\d\s]+', '', line)
+        
+        tokenized_sentences.append(line.lower().split())
+        stop_words = set(stopwords.words('portuguese') + list(punctuation))
+        for t in tokenized_sentences:
+            intSentences.append([w for w in t if w not in stop_words])
+
+intSentences = [w for w in intSentences if len(w) != 0]
+
+# # In[12]:
 
 means = []
 variances = []
@@ -116,79 +124,104 @@ for emd, i in zip(emdSentences, files):
     prefixo_emenda = i.split('.txt')[0]
     files_read.append(prefixo_emenda)
 
+    f= open("distance.txt","a+")
     print("abrindo emenda:", prefixo_emenda)
-    distances = []
-
+    f.write(str(prefixo_emenda) + "\n")
+    
+    min_teor_sentence_dist = 99999999999999
+    min_teor_sentence = ""
     for teor, j in zip(intSentences, range(len(intSentences))):
-        for teorWord in teor:
-            for emdWord in emd:
-                try:
-                    d = model.distance(emdWord, teorWord)
-                    if float("inf") == d:
-                        continue
-
-                    # Distância para calcular a média
-                    distances.append(d)
-                    # dists é uma lista de todas as distâncias calculadas
-                    indexes.append('_'.join([i, str(j)]))
-                    distancesDict[emdWord].append(distances)
-                except Exception:
-                    print(traceback.format_exc())
+        if len(emd) > 4 and len(teor)> 4:
+            d = model.wmdistance(emd,teor)
+            if float("inf") == d:
+                continue
+            if (d < min_teor_sentence_dist):
+                min_teor_sentence_dist = d
+                min_teor_sentence = teor
 
 
-    print(distancesDict.keys())
+    for teorWord in min_teor_sentence:
+        for emdWord in emd:
+            try:
+                d = model.distance(emdWord, teorWord)
+                if float("inf") == d:
+                    continue
+
+                # dists é uma lista de todas as distâncias calculadas
+                indexes.append('_'.join([i, str(j)]))
+                distancesDict[emdWord].append(d)
+            except Exception:
+                continue
+    #print(distancesDict.keys())
+    
+    f.write(str(emd) + "\n")
+    f.write("Linha do avulso com menor distância: " + str(min_teor_sentence) + "\n")
+    f.write(str(distancesDict.keys()) + "\n")
+    max_distance = -1
+    max_mean = -1
+    max_median = -1
     for key in distancesDict:
         distances = np.array(distancesDict[key])
         distancesDict[key] = distances
 
         allDists.extend(distances)
         soma = distances.sum()
+        if(soma > max_distance):
+            max_distance = soma
+            max_key = key
         mean = distances.mean()
-        # var = distances.var()
-        # std = math.sqrt(distances.var())
-        # sqdmean = 0
+        if(mean > max_mean):
+            max_mean = mean
+            max_mean_key = key
+        median = np.median(distances)
+        if(median > max_median):
+            max_median = median
+            max_median_key = key
+        var = distances.var()
+        std = math.sqrt(distances.var())
+        totdist = distances.sum()
 
-        # if len(distances) != 0:
-        #     for i in distances:
-        #         sqdmean += i**2
-        #     sqdmean = sqdmean/len(distances)
-        # totdist = distances.sum()
+        # f.write(("=======================================================\n"))
+        #f.write(str(key) + "\n")
+        #f.write("Soma: " + str(soma) + "\n")
+        #f.write("Média: " + str(mean) + "\n")
+        #f.write("Mediana: " + str(median) + "\n")
+        #f.write("Variação: " + str(var) + "\n")
+        #f.write("Desvio padrão: " + str(std) + "\n")
+        means.append(mean)
+        variances.append(var)
+        stds.append(std)
+        totdists.append(totdist)
+    f.write("Palavra mais distante do texto da proposição: " + str(max_key) + "\n")
+    f.write("Palavra com maior média: " + str(max_mean_key) + "\n")
+    f.write("Palavra com maior mediana: " + str(max_median_key) + "\n")
+    f.write(("=======================================================\n"))
+    f.close()
+    distancesDict = defaultdict(list)
 
-        print("=======================================================")
-        print(key)
-        print(soma)
-        print(mean)
-        # print(var)
-        # print(std)
-        # means.append(mean)
-        # variances.append(var)
-        # stds.append(std)
-        # sqdmeans.append(sqdmean)
-        # totdists.append(totdist)
+# allDists = np.array(allDists)
 
-allDists = np.array(allDists)
-
-indexes = np.array(indexes)
-
-
-def word2vec(word):
-    from collections import Counter
-    from math import sqrt
-
-    cw = Counter(word)
-    sw = set(cw)
-    lw = sqrt(sum(c*c for c in cw.values()))
-
-    # return a tuple
-    return cw, sw, lw
+# indexes = np.array(indexes)
 
 
-def cosdis(v1, v2):
-    common = v1[1].intersection(v2[1])
-    return sum(v1[0][ch]*v2[0][ch] for ch in common)/v1[2]/v2[2]
+# def word2vec(word):
+#     from collections import Counter
+#     from math import sqrt
+
+#     cw = Counter(word)
+#     sw = set(cw)
+#     lw = sqrt(sum(c*c for c in cw.values()))
+
+#     # return a tuple
+#     return cw, sw, lw
 
 
-wordtuples = []
+# def cosdis(v1, v2):
+#     common = v1[1].intersection(v2[1])
+#     return sum(v1[0][ch]*v2[0][ch] for ch in common)/v1[2]/v2[2]
+
+
+# wordtuples = []
 # for key in max_teor:
 #   for word in max_emd:
 #      try:
@@ -206,8 +239,8 @@ wordtuples = []
 # df = pd.DataFrame(np.column_stack([files_read, means, variances, stds, sqdmeans, totdists]),
 #	          columns=['files','mean_distance', 'variance', 'standard_deviation', 'sqd_means', 'total_distances']).to_csv(str(id_proposicao) + '_features_inter.csv')
 
-prop_emendas_dists = pd.DataFrame(np.column_stack([indexes, allDists]),
-                                  columns=['comparacao', 'distancia'])
-prop_emendas_dists.index.name = 'index'
-prop_emendas_dists.to_csv(
-    str(outputPath) + str(id_proposicao) + '_all_dist.csv')
+# prop_emendas_dists = pd.DataFrame(np.column_stack([indexes, allDists]),
+#                                   columns=['comparacao', 'distancia'])
+# prop_emendas_dists.index.name = 'index'
+# prop_emendas_dists.to_csv(
+#     str(outputPath) + str(id_proposicao) + '_all_dist.csv')
